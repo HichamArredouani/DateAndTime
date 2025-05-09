7fd7e9b02485c5a8ca45421ee8a06e5dc7a3151b
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-09 20:04:31 +0200

    Owntwo

--- src/Owntwo.java
+++ src/Owntwo.java
@@ -2,6 +2,7 @@ import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.LocalDate;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class Owntwo {
     public static void main (String[] args) {
@@ -9,5 +10,9 @@ public class Owntwo {
         LocalDateTime updatedMeetingTime = meetingTime.plusHours(2).plusMinutes(30);
 
         System.out.println(meetingTime);
+
+        LocalDate a = LocalDate.of(2023, 1, 1);
+        LocalDate b = LocalDate.of(2023, 2, 15);
+        System.out.println(ChronoUnit.DAYS.between(a, b));
     }
 }
